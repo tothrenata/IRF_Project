d5a5a69492ae0f66b0d5643e1d0ddbc64ce53281
Renáta Tóth <renata.toth@stud.uni-corvinus.hu>
2020-12-13 10:08:55 +0100

    Property-k és enumeráció deklarálása

--- Project_Z8PGPQ/Project_Z8PGPQ/CostCenter.cs
+++ Project_Z8PGPQ/Project_Z8PGPQ/CostCenter.cs
@@ -8,5 +8,26 @@ namespace Project_Z8PGPQ
 {
     class CostCenter
     {
+        public String CTR { get; set; }
+        public DateTime VFROM { get; set; }
+        public DateTime VTO { get; set; }
+        public String TYPE { get; set; }
+        public String PROFCTR { get; set; }
+        public OrgCodes ORGCODE;
+        public string ORGCODE_STR
+        {
+            get
+            {
+                if (ORGCODE == OrgCodes.FI) return "FI";
+                if (ORGCODE == OrgCodes.BI) return "BI";
+                if (ORGCODE == OrgCodes.MA) return "MA";
+                return "invalid";
+            }
+            set
+            {
+                ORGCODE = (OrgCodes)int.Parse(value);
+            }
+        }
+        public String GEOCODE { get; set; }
     }
 }
---
+++ Project_Z8PGPQ/Project_Z8PGPQ/OrgCodes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Z8PGPQ
+{
+    public enum OrgCodes
+    {
+        FI=1,
+        BI=2,
+        MA=3
+    }
+}
